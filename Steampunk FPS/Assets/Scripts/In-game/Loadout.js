60b6d93a54ed3056a5388349b3197b1a65b744e3
Andreawse <andreas@edal.dk>
2015-09-28 21:43:51 +0200

    Begyndt på loadout koden

---
+++ Steampunk FPS/Assets/Scripts/In-game/Loadout.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI () {
+	
+	//Baggrundsboxen
+	GUI.Box(new Rect(ScrInPro(5,"width"), ScrInPro(7,"height"), ScrInPro(30,"width"), ScrInPro(90,"height")), "");
+	
+	//Button
+	if (GUI.Button(new Rect(ScrInPro(6,"width"), ScrInPro(10,"height"), ScrInPro(28,"width"), ScrInPro(30,"height")), "Click me")) {
+    	Debug.Log("Clicked the button with text");
+    }
+	
+}
+
+//En function som tager en procentdel af skærmopløsningen
+function ScrInPro (procent : int, orientation : String) {
+	
+	if (orientation == "height") {
+		return Screen.height/100*procent;
+	}
+	
+	if (orientation == "width") {
+		return Screen.width/100*procent;
+	}
+	
+}
