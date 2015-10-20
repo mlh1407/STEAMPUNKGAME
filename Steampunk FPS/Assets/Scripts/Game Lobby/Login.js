7afa9b088d7dc7b9c9e8b8bdfb16072bf3cb9f01
oliv2112 <oli@nordestgaard.org>
2015-10-20 17:28:38 +0200

    Lobby

---
+++ Steampunk FPS/Assets/Scripts/Game Lobby/Login.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+var Username : Text;
+var Password : Text;
+
+function LoadOnClick () {
+	if(Username.text == "oliver"){
+	Debug.Log("JAAAA");
+		if(Password.text == "1234"){
+		Debug.Log("mere JA");
+		}
+		else{
+		Debug.Log("næsten der");
+		}
+	}
+	else{
+	Debug.Log("Neeeeej");
+	}
+	//Debug.Log(Username.text); 
+}
+
+function Update () {
+
+}
