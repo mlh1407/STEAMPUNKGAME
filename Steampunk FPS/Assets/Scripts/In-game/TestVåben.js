2dd6b76cc05ea2f6ee2f611555f6a7cd8ddd75e6
Andreawse <andreas@edal.dk>
2015-11-16 21:32:39 +0100

    Lightmap til testbane

---
+++ Steampunk FPS/Assets/Scripts/In-game/TestVåben.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var particleEmitters : ParticleSystem[];
+ 
+function Start () {
+
+}
+
+function Update () {
+
+	if (Input.GetButtonDown("Fire1")) {
+		for (var i = 0; i < particleEmitters.length; i++) {
+			
+			particleEmitters[i].Play();
+		}
+	}
+
+}
